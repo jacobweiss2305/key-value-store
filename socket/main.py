688d5bc9ab172f8ffb53258501dd9113286940b6
jacob weiss <jacob.weiss@data-axle.com>
2023-07-15 10:40:36 -0600

    update

--- socket/main.py
+++ socket/main.py
@@ -1,9 +1,10 @@
 import socket
 import threading
 import json
+import uuid
 
 class TransactionServer:
-    def __init__(self, host = 'localhost', port = 12345):
+    def __init__(self, host='localhost', port=12345):
         """Initialize the server with a host and port to listen to."""
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((host, port))
@@ -14,14 +15,14 @@ class TransactionServer:
     def load_data(self):
         """Load the main data store from a file."""
         try:
-            with open('database.json', 'r') as f:
+            with open('db.json', 'r') as f:
                 return json.load(f)
         except FileNotFoundError:
             return {}
         
     def save_data(self):
         """Save the main data store to a file."""
-        with open('database.json', 'w') as f:
+        with open('db.json', 'w') as f:
             json.dump(self.data, f)
         
     def handle_client(self, client):
@@ -54,18 +55,26 @@ class TransactionServer:
         id = command.get('id')
 
         if cmd == 'START':
+            # Generate a new transaction ID
+            transaction_id = str(uuid.uuid4())
             # Starts a new transaction (Atomicity)
-            self.transactions[id] = {}
-            return {'status': 'Ok', 'mesg': 'Transaction started'}
+            self.transactions[transaction_id] = {}
+            return {'status': 'Ok', 'mesg': 'Transaction started', 'transaction_id': transaction_id}
         elif cmd == 'COMMIT':
             # Commit a transaction (Atomicity, Durability)
-            self.data.update(self.transactions.pop(id, {}))
-            self.save_data()  # Durability: save committed changes to a file
-            return {'status': 'Ok', 'mesg': 'Transaction committed'}
+            if id in self.transactions:
+                self.data.update(self.transactions.pop(id, {}))
+                self.save_data()  # Durability: save committed changes to a file
+                return {'status': 'Ok', 'mesg': 'Transaction committed'}
+            else:
+                return {'status': 'Error', 'mesg': 'Transaction not found'}
         elif cmd == 'ROLLBACK':
             # Roll back a transaction (Atomicity)
-            self.transactions.pop(id, None)
-            return {'status': 'Ok', 'mesg': 'Transaction rolled back'}
+            if id in self.transactions:
+                self.transactions.pop(id, None)
+                return {'status': 'Ok', 'mesg': 'Transaction rolled back'}
+            else:
+                return {'status': 'Error', 'mesg': 'Transaction not found'}
         elif cmd == 'PUT':
             # Add or update a key-value pair (Atomicity, Consistency)
             if id in self.transactions:
@@ -73,7 +82,7 @@ class TransactionServer:
             else:
                 self.data[key] = value
             return {'status': 'Ok', 'mesg': 'Key-value pair added/updated'}
-        elif cmd == 'DEL':
+        elif cmd == 'DELETE':
             # Delete a key-value pair (Atomicity, Consistency)
             if id in self.transactions and key in self.transactions[id]:
                 del self.transactions[id][key]
@@ -81,13 +90,20 @@ class TransactionServer:
                 del self.data[key]
             return {'status': 'Ok', 'mesg': 'Key-value pair deleted'}
         elif cmd == 'GET':
-            # Get the value for a key (Atomicity)
-            if id in self.transactions and key in self.transactions[id]:
-                return {'status': 'Ok', 'result': self.transactions[id].get(key)}
+            # Get the value for a key or retrieve the entire database
+            if key:
+                if id in self.transactions and key in self.transactions[id]:
+                    return {'status': 'Ok', 'result': self.transactions[id].get(key)}
+                else:
+                    return {'status': 'Ok', 'result': self.data.get(key)}
             else:
-                return {'status': 'Ok', 'result': self.data.get(key)}
+                return {'status': 'Ok', 'result': self.data}
+        elif cmd == 'VIEW':
+            # Retrieve and return the entire database
+            return {'status': 'Ok', 'result': self.data}
         else:
             return {'status': 'Error', 'mesg': 'Unknown command'}
+
 
     def start(self):
         """Start the server."""
